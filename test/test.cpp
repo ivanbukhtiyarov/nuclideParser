90f179d41bccf8edff4baaddb9af6b80986167f2
dr.yuri <dr.yuri92@gmail.com>
2019-11-25 17:11:24 +0300

    work flow

--- test/test.cpp
+++ test/test.cpp
@@ -7,7 +7,7 @@
 // Tests that the Foo::Bar() method does Abc.
 TEST(ChainTest, test1) {
   using namespace openbps;
-  const std::string input_filepath = "../Xmls/chain_simple.xml";
+  const std::string input_filepath = "./Xmls/chain_simple.xml";
   Chain chain(read_xml(input_filepath));
   auto react_map = chain.form_reaction();
   EXPECT_EQ(react_map["(n,gamma)"].size(), 4);
@@ -16,7 +16,7 @@ TEST(ChainTest, test1) {
 // Tests that the Foo::Bar() method does Abc.
 TEST(ChainTest, test2) {
   using namespace openbps;
-  const std::string input_filepath = "../Xmls/chain_simple.xml";
+  const std::string input_filepath = "./Xmls/chain_simple.xml";
   Chain chain(read_xml(input_filepath));
   auto mapc = chain.form_yield_map();
   EXPECT_EQ(mapc["0.0253"][0][0], 6);
