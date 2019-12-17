bc9c6971cba2ba4f394949933fd04d74c79be2c3
dr.yuri <dr.yuri92@gmail.com>
2019-12-17 18:24:03 +0300

    work in progress

--- src/reactions.cpp
+++ src/reactions.cpp
@@ -3,24 +3,69 @@
 #include <map>
 #include <iostream>
 #include <string.h>
+#include "configure.h"
+#include "nuclide_class.h"
+#include "../extern/pugiData/pugixml.h"
 
 namespace openbps {
 
+std::vector<Composition> compositions;
+std::map<std::string, int> composmap;
+size_t indexall;
+
 Composition::Composition(pugi::xml_node node){
 
+	if (check_for_node(node, "name")) {
+	    this->name = node.attribute("name").value();
+	}
+	if (check_for_node(node, "energy")) {
+		for (pugi::xml_node tool : node.children("energy")) {
+            size_t cng = std::stoi(tool.attribute("ng").value());
+            this->energies_.insert({cng, get_node_array<double>(tool, "energy")})
+            this->energy_number++;
+		}
+	}
+	if (check_for_node(node, "namenuclides")) {
+	    this->namenuclides_ = get_node_array<std::string>(node, "namenuclides");
+	}
+	if (check_for_node(node, "flux")) {
+	    this->flux_ = get_node_array<std::double>(node, "flux");
+	}
+	if (check_for_node(node, "spectrum")) {
+	    this->spectrum_ = get_node_array<std::double>(node, "spectrum");
+	}
+	if (check_for_node(node, "conc")) {
+		this->conc_ = get_node_array<std::double>(node, "conc");
+	}
+
+
+	pugi::xml_node child_node {node.child("flux")};
+
+
 }
 
-s_nuclides_ Composition::parse_nuclides_xml_(pugi::xml_node node){
+void Composition::parse_compos_xml_(pugi::xml_node node){
 
 }
-    s_xs_       Composition::parse_xs_xml_(pugi::xml_node node){
+
+s_xs_ Composition::parse_xs_xml_(pugi::xml_node node){
 
 }
 
 void read_reactions_xml(){
-	//using namespace openbps;
+	pugi::xml_document doc;
+	auto result = doc.load_file(configure::reaction_file.c_str());
+	if (!result) {
+	    std::cerr << "Error: file not found!" << std::endl;
+	}
+	pugi::xml_node root_node = doc.child("compositions");
 
 	std::cout << "I' m in reactions.xml parser" << std::endl;
+
+	for (pugi::xml_node tool : root_node.children("composit")) {
+		compositions.push_back(tool);
+	}
+
 
 }
 
--- src/reactions.h
+++ src/reactions.h
@@ -7,21 +7,22 @@
 
 namespace openbps {
 
-struct s_nuclides_ {
 
-    std::string nuclide_name;
-    double nuclide_conc;
-    int nuclide_ind;
-    double nuclide_mass;
+//?
+//struct s_nuclides_ {
 
-};
+//    std::string nuclide_name;
+//    double nuclide_conc;
+//    int nuclide_ind;
+//    double nuclide_mass;
+
+//};
 
 struct s_xs_ {
-
-
-
-    std::map<std::string, std::string, std::vector<double>> rxs_;
-    std::map<std::string, std::string, std::vector<double>> xs_;
+    std::string xsname;
+    std::string xstype;
+    std::vector<double> rxs_;
+    std::vector<double> xs_;
 };
 
 class Composition {
@@ -34,16 +35,24 @@ public:
     ~Composition() {std::cout << "Composition " + name + " destroyed\n";};
 
 private:
-    std::vector<s_nuclides_>  nuclides_;
+    //std::vector<s_nuclides_>  nuclides_;
     std::vector<s_xs_>  xslib_;
-    std::vector<double> energies_;
+    std::map<size_t, std::vector<double>> energies_;
+    std::vector<std::string> namenuclides_;
     std::vector<double> spectrum_;
+    std::vector<double> conc_;
     std::vector<double> flux_;
-    s_nuclides_ parse_nuclides_xml_(pugi::xml_node node);
-    s_xs_       parse_xs_xml_(pugi::xml_node node);
+    void parse_compos_xml_(pugi::xml_node node);
+    s_xs_ parse_xs_xml_(pugi::xml_node node);
+
+}
+
 
 
 void read_reactions_xml();
-
+extern std::vector<Composition> compositions;
+extern std::map<std::string, int> composmap;
+extern size_t indexall;
 }
+
 #endif /* SRC_REACTIONS_H_ */
