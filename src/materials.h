15887130495c82061b41d9b8927ae85752350c0a
dryuri92 <39188804+dryuri92@users.noreply.github.com>
2020-05-15 01:09:09 +0300

    Update materials.h

--- src/materials.h
+++ src/materials.h
@@ -3,6 +3,36 @@
 #include <vector>
 #include <map>
 #include <iostream>
+#include "reactions.h"
+#include "../extern/pugiData/pugixml.h"
+
+class Materials {
+    public:
+        Materials();
+        void xml_add_material(pugi::xml_node node);
+        void check_node(pugi::xml_node node);
+        std::string name;
+        std::vector<std::string> namenuclides;
+        std::vector<double> conc;
+        double volume;
+        double mass;
+        double power;
+        Composition* compos;
+        void bindcomposition(Composition& extcompos){compos=&extcompos};
+        
+};
+
+void form_materials_xml(std::vector<Materials> m_arr, std::string xml_path);
+std::vector<Materials> read_materials_from_reactions();
+std::vector<Materials> read_materials_from_inp(std::string inp_path);
+std::vector<Materials> parse_xml_materials();
+void matchcompositions(std::vector<Composition>& compositions, std::vector<Materials>& materials);
+
+#endif /* SRC_MATERIALS_H_ */#ifndef SRC_MATERIALS_H_
+#define SRC_MATERIALS_H_
+#include <vector>
+#include <map>
+#include <iostream>
 #include "../extern/pugiData/pugixml.h"
 
 class Materials {
