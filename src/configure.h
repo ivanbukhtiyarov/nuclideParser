e1e25040587d4ab8304b431f67c06a86a70f87c0
dryuri92 <39188804+dryuri92@users.noreply.github.com>
2020-05-15 01:05:00 +0300

    Update configure.h

--- src/configure.h
+++ src/configure.h
@@ -20,6 +20,44 @@ extern int numstep;                       //!< number of substep per one time st
 extern double timestep ;                  //!< length of time interval for decesion
 extern bool somebool;                     //!< some bool variable
 extern pugi::xml_document docx;           //!< file handle for chain.xml
+extern enum mode {exponent, iteration, chebyshev};
+
+}
+
+//! Parse init line
+int parse_command_line(int argc, char* argv[]);
+
+//! Read configure from XML file
+void read_conigure_xml();
+
+//! Read input XML files
+void read_input_xml();
+
+}
+
+#endif /* SRC_CONFIGURE_H_ */
+#ifndef SRC_CONFIGURE_H_
+#define SRC_CONFIGURE_H_
+
+#include "../extern/pugiData/pugixml.h"
+
+namespace openbps {
+
+//==============================================================================
+// Global variable declarations
+//==============================================================================
+
+namespace configure {
+
+extern std::string path_input;            //!< directory where main .xml files resides
+extern std::string path_output;           //!< directory where output files are written
+extern std::string chain_file;            //!< chain-filename.xml
+extern std::string reaction_file;         //!< reaction-filename.xml
+extern int someint;                       //!< some int variable
+extern int numstep;                       //!< number of substep per one time step
+extern double timestep ;                  //!< length of time interval for decesion
+extern bool somebool;                     //!< some bool variable
+extern pugi::xml_document docx;           //!< file handle for chain.xml
 
 
 }
