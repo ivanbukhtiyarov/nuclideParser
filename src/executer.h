7133554377e173e1eeddb60e0b2418d74a961639
dryuri92 <39188804+dryuri92@users.noreply.github.com>
2020-05-15 01:07:25 +0300

    Update executer.h

--- src/executer.h
+++ src/executer.h
@@ -1,5 +1,8 @@
+
 #ifndef SRC_EXECUTER_H_
 #define SRC_EXECUTER_H_
+#include "../extern/xtensor/include/xtensor/xarray.hpp"
+#include "../extern/xtensor/include/xtensor/xadapt.hpp"
 
 namespace openbps {
 
@@ -8,6 +11,11 @@ namespace executer {
 void run_solver();
 
 void init_solver();
+
+void exponental(xt::xarray<double>& matrix, xt::xarray<double>& y);
+ 
+void iterative(xt::xarray<double>& matrix, xt::xarray<double>& y);
+void cram(xt::xarray<double>& matrix, xt::xarray<double>& y);
 }
 
 }
